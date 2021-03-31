325756e5a7e4a8633fe3d0b28997945638b73855
lusatiro <ljsatiro@gmail.com>
2021-03-31 01:03:43 -0300

    add instance type and size

--- lib/eks-cluster-stack.ts
+++ lib/eks-cluster-stack.ts
@@ -1,13 +1,16 @@
 import * as cdk from '@aws-cdk/core';
-import { Cluster, KubernetesVersion } from '@aws-cdk/aws-eks';
+import { Cluster, KubernetesVersion,  } from '@aws-cdk/aws-eks';
+import { InstanceType, InstanceClass, InstanceSize  } from '@aws-cdk/aws-ec2';
 
 export class EksClusterStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
+    
     const cluster = new Cluster(this, 'EksCluster', {
       clusterName: 'eks-cluster',
       version: KubernetesVersion.V1_19,
+      defaultCapacity: 2,
+      defaultCapacityInstance: InstanceType.of(InstanceClass.T2, InstanceSize.SMALL)
     });
   }
 }
